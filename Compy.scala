fc5980b87487b25e3c5ddc3360f53a1c69bfbf8e
brendon-boldt <brendon_boldt@yahoo.com>
2016-02-06 01:24:11 -0500

    Adds comments; consolidates grammar

--- Compy.scala
+++ Compy.scala
@@ -8,6 +8,9 @@ object Main {
     g.addKind("type",     """\b(int|string|boolean)\b""".r)
     g.addKind("int",      """(\b0|[1-9][\d]*\b)""".r)
     g.addKind("boolval",  """(\btrue|false\b)""".r)
+    g.addKind("ctrlflow", """(\bif|while\b)""".r)
+    g.addKind("symbol",   """[\(\)\{\}=\+\$]|(==)|(!=)""".r)
+    /*
     g.addKind("if",       """\bif\b""".r)
     g.addKind("while",    """\bwhile\b""".r)
     g.addKind("lparen",   """\(""".r)
@@ -18,15 +21,17 @@ object Main {
     g.addKind("eq",       """==""".r)
     g.addKind("neq",      """!=""".r)
     g.addKind("plus",     """\+""".r)
+    g.addKind("eop",      """\$""".r)
+    */
     g.addKind("print",    """(print\((.*)\))""".r)
     g.addKind("stringlit","""(\"([^"]*)\")""".r)
-    g.addKind("eop",      """\$""".r)
     g.addKind("newline",  """\n""".r)
     g.addKind("id",       """\b([A-z][\w]*)\b""".r)
     g.addKind("ws",       """[\r\t\f ]+""".r)
     val l = new Lexer(g)
 
     l.string = Source.fromFile(args(0)).toStream.mkString
-    l.getTokenIterator.foreach((s:Token) => print(s.string))
+    l.getTokenIterator
+    //l.getTokenIterator.foreach((s:Token) => println(s.string))
   }
 }
--- Lexer.scala
+++ Lexer.scala
@@ -4,42 +4,66 @@ import scala.util.matching.Regex.Match
 import scala.collection.mutable.ArrayBuffer
 
 class Lexer(val grammar: Grammar) {
+  // A string which will contain the source file bytes
   var string = ""
+  // Number of exactly what you would think
   var errors = 0
 
-  def removeDuplicates(ab: ArrayBuffer[Token]): ArrayBuffer[Token] = {
+  /** Filters token stream of unwanted tokens.
+   *  
+   *  Removes identifiers that are actually keyword and whitespace tokens.
+   */
+  private def filterTokens(tokens: ArrayBuffer[Token]): ArrayBuffer[Token] = {
     val filtered = new ArrayBuffer[Token]
+    // Tracks where in the source file the tokens are
     var index = 0 
+    // Tracks the line number for error reporting
     var line = 1
-    for ( token <- ab ) {
+    for ( token <- tokens ) {
+      // Set the token's line number
       token.line = line
       if ( token.kind.name == "newline" ) {
         line += 1
       }
       if (index <= token.start) {
+        // Illegal characters such as @ or # will cause a gap in the token stream.
+        // This means the index will be less than the start of the next token.
         if (index < token.start) {
           println("Unrecognized token " + string(index) + " at line " + token.line)
           index = token.start
           errors += 1
         }
-        filtered += token
+        // Only add a token if it is not whitespace
+        if (!(token.kind.name == "ws" || token.kind.name == "newline"))
+          filtered += token
+        // Advance the index
         index += token.length
       }
     }
     filtered
   }
 
+  /** Processes the source file string using the RegEx-definied grammar.
+   *
+   */
   def getTokenIterator: Iterator[Token] = {
+    // Reset the number of errors
     errors = 0
     var tokens = new ArrayBuffer[Token]
+    // Loop through each of the kinds as defined by the grammar
     for ( k <- grammar.kinds ) {
+      // Save all RegEx matches
       var matched = k.regex.findAllMatchIn(string)
       matched.foreach((m: Match) => {
+        // Create a token from the match
         var token = new Token(k, m)
         tokens += token
       })
     }
+    // Sort the token by position in the source file
     val sorted = tokens.sortBy(_.start)
-    return removeDuplicates(sorted).iterator
+    // Filter the token stream and return the iterator
+    return filterTokens(sorted).iterator
   }
+
 }
